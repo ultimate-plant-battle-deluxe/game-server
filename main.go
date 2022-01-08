6cfc00a8e1f0443b2f917e05c92ab61192f58ab8
Wyatt Arent <hello@wyatt.engineer>
2022-01-08 12:29:44 -0700

    Hella server updates

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"math/rand"
 	"net/http"
 	"time"
+
+	b64 "encoding/base64"
 
 	"github.com/golang-jwt/jwt"
 	uuid "github.com/satori/go.uuid"
@@ -22,20 +23,26 @@ type GameState struct {
 	Items []int `json:"items"`
 }
 
-var ItemCount = 3
+var ItemOdds []int = []int{
+	1000, // Leaf
+	1000, // Water
+	250, // Seed
+}
 
-func start(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	gameState := GameState{
-		Id: uuid.NewV4(),
-		Time: 10,
-		Items: []int{},
+func rollItem() int {
+	itemRoll := RandomInt(0, ItemOdds[len(ItemOdds)-1])
+	for itemId, prob := range ItemOdds {
+		if itemRoll <= prob {
+			return itemId
+		}
 	}
-	for i := 0; i < 3; i++ {
-		gameState.Items = append(gameState.Items, RandomInt(0, ItemCount-1))
-	}
+	// Should never happen
+	fmt.Println("Probability error")
+	return 0
+}
 
-	state, err := json.Marshal(gameState)
+func writeStateToken(w http.ResponseWriter, gs *GameState) {
+	state, err := json.Marshal(gs)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -43,7 +50,7 @@ func start(w http.ResponseWriter, req *http.Request) {
 	// you would like it to contain.
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"state": state,
-		"exp": time.Now().Add(time.Hour * 24),
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
 	})
 	tokenString, err := token.SignedString([]byte("yolo"))
 	if err != nil {
@@ -52,18 +59,52 @@ func start(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("x-token", tokenString)
 }
 
-func roll(w http.ResponseWriter, req *http.Request) {
+func parseStateToken(req *http.Request) *GameState {
+	gameState := &GameState{}
+
+	token, err := jwt.Parse(req.Header.Get("X-Token"), func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("yolo"), nil
+	})
+	if err != nil {
+		fmt.Println("Error", err)
+		return gameState
+	}
+	claims := token.Claims.(jwt.MapClaims);
+	sDec, _ := b64.StdEncoding.DecodeString(claims["state"].(string))
+	gameState = &GameState{}
+	json.Unmarshal([]byte(sDec), gameState)
+	return gameState
+}
+
+func start(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	gameState := GameState{
+	gameState := &GameState{
+		Id: uuid.NewV4(),
+		Time: 10,
 		Items: []int{},
 	}
 	for i := 0; i < 3; i++ {
-		gameState.Items = append(gameState.Items, RandomInt(1, ItemCount))
+		gameState.Items = append(gameState.Items, rollItem())
 	}
 
-	err := json.NewEncoder(w).Encode(gameState)
-	if err != nil {
-		log.Fatalln(err)
+	writeStateToken(w, gameState)
+}
+
+func roll(w http.ResponseWriter, req *http.Request) {
+	gameState := parseStateToken(req)
+	defer writeStateToken(w, gameState)
+
+	if gameState.Time < 1 {
+		return
+	}
+	
+	gameState.Time -= 1
+	gameState.Items = []int{}
+	for i := 0; i < 3; i++ {
+		gameState.Items = append(gameState.Items, rollItem())
 	}
 }
 func consume(w http.ResponseWriter, req *http.Request) {
@@ -78,6 +119,15 @@ func battle(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 		rand.Seed(time.Now().UnixNano())
+
+		// Compile probabilities
+		for idx := range ItemOdds {
+			if idx == 0 {
+				continue
+			}
+
+			ItemOdds[idx] += ItemOdds[idx - 1]
+		}
 
     http.HandleFunc("/v1/start", start)
     http.HandleFunc("/v1/roll", roll)
